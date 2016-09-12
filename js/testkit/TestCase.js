400762b783fd95b61f37dc7bc88349ab2de18986
maxbilbow <UJ2-rnb-KSV-qNf>
2016-09-12 23:53:45 +0100

    test kit

--- js/testkit/TestCase.js
+++ js/testkit/TestCase.js
@@ -2,99 +2,97 @@
  * Created by max on 09/09/16.
  */
 
-var TestKit = function () {
-    this.tests = {};
-    this.count = 0;
-}
+(function() {
+    "use strict";
 
-TestKit.prototype.tests = null;
-
-
-TestKit.prototype.assertTrue = function (a,aMessage) {
-    if (a !== true)
-    {
-        throw Error(aMessage || 'false');
-    }
-};
-
-
-TestKit.prototype.assertFalse = function (a,aMessage) {
-    if (a !== false)
-    {
-        throw Error(aMessage || 'true');
-    }
-};
-
-TestKit.prototype.assertEquals = function (a,b,aMessage) {
-    this.assertTrue(a === b, a+ ' !== ' + b + ' ' + (aMessage||''));
-};
-
-TestKit.prototype.assertNotEquals = function (a,b,aMessage) {
-    this.assertFalse(a === b, a+ ' === ' + b + ' ' + (aMessage||''));
-};
-
-
-TestKit.prototype.assertEquivalent = function (a,b,aMessage) {
-    this.assertTrue(a == b, a+ ' != ' + b + ' ' + (aMessage||''));
-};
-
-TestKit.prototype.assertNotEquivalent = function (a,b,aMessage) {
-    this.assertFalse(a == b, a+ ' == ' + b + ' ' + (aMessage||''));
-};
-
-
-TestKit.prototype.addTest = function (aName,aTest) {
-    if (aTest === undefined)
-    {
-        aTest = aName;
-        aName = 'Test'+this.count++;
-    }
-    else
-    {
-        aName = 'Test'+this.count++ + '_' + aName;
-    }
-    var $this = this;
-    this.tests[aName] = function () {
-        aTest($this);
+    var TestKit = function () {
+        this.tests = {};
+        this.count = 0;
     };
-    return this;
-}
 
-TestKit.prototype.run = function () {
-    var tests = this.tests;
-    Object.keys(tests).forEach(function (aName)
-    {
-        try {
-           tests[aName]();
-            console.info(aName,'PASSED');
+    TestKit.prototype.tests = null;
+
+
+    TestKit.prototype.assertTrue = function (a, aMessage) {
+        if (a !== true) {
+            throw Error(aMessage || 'false');
         }
-        catch (e)
-        {
-            console.error(aName,'FAILED',e);
+    };
+
+
+    TestKit.prototype.assertFalse = function (a, aMessage) {
+        if (a !== false) {
+            throw Error(aMessage || 'true');
         }
+    };
 
-    });
-}
+    TestKit.prototype.assertEquals = function (a, b, aMessage) {
+        this.assertTrue(a === b, a + ' !== ' + b + ' ' + (aMessage || ''));
+    };
 
-var test = new TestKit();
-
-test.addTest('first',function ($) {
-    $.assertEquals(1,1);
-    $.assertNotEquals(1,'1');
-    $.assertEquivalent(1,'1');
-    $.assertNotEquivalent(1,'2');
-
-    $.assertEquivalent(null,undefined);
-    $.assertNotEquals(null,undefined);
-
-    $.assertEquivalent(false, 0);
-    $.assertNotEquals(false,0);
-
-    $.assertEquivalent(0, []);
-    $.assertNotEquals(0,[]);
-
-    $.assertEquivalent(false, []);
-    $.assertNotEquals(false,[]);
+    TestKit.prototype.assertNotEquals = function (a, b, aMessage) {
+        this.assertFalse(a === b, a + ' === ' + b + ' ' + (aMessage || ''));
+    };
 
 
-}).run();
+    TestKit.prototype.assertEquivalent = function (a, b, aMessage) {
+        this.assertTrue(a == b, a + ' != ' + b + ' ' + (aMessage || ''));
+    };
+
+    TestKit.prototype.assertNotEquivalent = function (a, b, aMessage) {
+        this.assertFalse(a == b, a + ' == ' + b + ' ' + (aMessage || ''));
+    };
+
+
+    TestKit.prototype.addTest = function (aName, aTest) {
+        if (aTest === undefined) {
+            aTest = aName;
+            aName = 'Test' + this.count++;
+        }
+        else {
+            aName = 'Test' + this.count++ + '_' + aName;
+        }
+        var $this = this;
+        this.tests[aName] = function () {
+            aTest($this);
+        };
+        return this;
+    };
+
+    TestKit.prototype.run = function () {
+        var tests = this.tests;
+        Object.keys(tests).forEach(function (aName) {
+            try {
+                tests[aName]();
+                console.info(aName, 'PASSED');
+            }
+            catch (e) {
+                console.error(aName, 'FAILED', e);
+            }
+
+        });
+    };
+
+    var test = new TestKit();
+
+    test.addTest('first', function ($) {
+        $.assertEquals(1, 1);
+        $.assertNotEquals(1, '1');
+        $.assertEquivalent(1, '1');
+        $.assertNotEquivalent(1, '2');
+
+        $.assertEquivalent(null, undefined);
+        $.assertNotEquals(null, undefined);
+
+        $.assertEquivalent(false, 0);
+        $.assertNotEquals(false, 0);
+
+        $.assertEquivalent(0, []);
+        $.assertNotEquals(0, []);
+
+        $.assertEquivalent(false, []);
+        $.assertNotEquals(false, []);
+    }).run();
+
+
+})();
