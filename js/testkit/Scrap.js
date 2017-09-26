13a92acf4286b509b2979e9bd53458fa78d6db67
maxbilbow <UJ2-rnb-KSV-qNf>
2017-09-26 18:37:00 +0100

    created scrap file with useful ie11 things

---
+++ js/testkit/Scrap.js
@@ -0,0 +1,15 @@
+/**
+ * Created by max on 23/09/17.
+ */
+
+
+/**
+ * For use with IE 11 & lower:
+ *
+
+<script type="text/javascript">
+if(/MSIE \d|Trident.*rv:/.test(navigator.userAgent))
+  document.write('<script src="https://cdnjs.cloudflare.com/ajax/libs/bluebird/3.3.4/bluebird.min.js"><\/script>');
+</script>
+
+ */
